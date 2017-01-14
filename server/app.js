1dfaa9244b0a597fe1dee581d9acb7b852ffc27c
Michael Dwyer <mcdwyer1@gmail.com>
2017-01-14 11:08:16 -0800

    Implement node cluster

--- server/app.js
+++ server/app.js
@@ -1,22 +1,37 @@
 var express = require('express');
 var db = require('./db');
-//var controller = require('./controllers');
+var cluster = require('cluster');
 
 var parser = require('body-parser');
 var morgan = require('morgan');
 
 var router = require('./routes.js');
 
-var app = express();
+if (cluster.isMaster) {
+  var cpus = require('os').cpus().length;
+  for (var i = 0; i < cpus; i += 1) {
+      cluster.fork();
+  }
+  cluster.on('exit', function(worker) {
+    console.log('worker ' + worker.id +  ' died');
+    cluster.fork();
+  });
+} else {
+  var app = express();
+  app.get('/', function (req, res) {
+      res.send('Hello World');
+  });
 
-app.set('port', 8000);
+  app.set('port', 8000);
 
-app.use(parser.json());
-app.use(morgan('dev'));
+  app.use(parser.json());
+  app.use(morgan('dev'));
 
-app.use('/', router);
+  app.use('/', router);
 
-app.listen(app.get('port'));
-console.log('Running on port ' + app.get('port'));
+  app.listen(app.get('port'));
+  console.log('Running on port ' + app.get('port'));
+}
+
 
 module.exports = app;
