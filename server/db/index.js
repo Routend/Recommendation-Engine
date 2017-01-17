15c71878399f13fff2b6ec116b56c6cb316c5fd4
Michael Dwyer <mcdwyer1@gmail.com>
2017-01-16 20:01:22 -0800

    Add remote db config

--- server/db/index.js
+++ server/db/index.js
@@ -2,8 +2,10 @@ var mysql = require('mysql');
 
 
 var connection = mysql.createConnection({
-  user: 'root',
-  password: '',
+  host: '107.170.226.9';
+  port: '3306';
+  user: 'mike',
+  password: 'routend',
   database: 'routend'
 });
 
